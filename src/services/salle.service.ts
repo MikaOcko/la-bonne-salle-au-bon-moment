ad41794ecd35a3eaa8b0c1c6979f5880f9e2eee7
Kimberly Moreau <moreau.magali.kimberly@gmail.com>
2026-09-23 11:54:15 +0200

    feat: :construction: add delete function for rooms

--- src/services/salle.service.ts
+++ src/services/salle.service.ts
@@ -1,19 +1,9 @@
 // ----------- Imports -----------
-
+// API URL (from DB)
 const API_URL = import.meta.env.VITE_API_URL;
 
 // ----------- Logic -----------
-
-export async function createSalle(room: object) {
-  const response = await fetch(`${API_URL}/rooms`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(room),
-  });
-
-  return response.json();
-}
-
+// Function to retrieve all rooms (into DB)
 export async function getSalles() {
   const response = await fetch(`${API_URL}/rooms`);
 
@@ -23,3 +13,33 @@ export async function getSalles() {
 
   return response.json();
 }
+
+// Funtion to create a new room
+export async function createRoom(room: object) {
+	const response = await fetch(`${API_URL}/rooms`, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(room),
+	});
+
+	return response.json();
+}
+
+// Function to delete one room
+export async function deleteRoom(id:string){
+	// if (!id) {
+    // 	throw new Error("Impossible de supprimer la salle : identifiant manquant.");
+ 	//  };
+
+	const response = await fetch(`${API_URL}/rooms/${id}`, {
+		method: "DELETE",
+	});
+	console.log(id);
+	console.log(response);
+	if (!response.ok) {
+    	throw new Error("Impossible de supprimer la salle.");
+  	}
+
+	return response;
+};
+
